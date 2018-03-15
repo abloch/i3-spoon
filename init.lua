fa2c72dc82bb75839421211d462248b02dd4b6f0
Akiva <bloch.akiva@gmail.com>
2018-03-15 02:24:10 +0200

    add resize commands

--- init.lua
+++ init.lua
@@ -8,8 +8,34 @@ hs.hotkey.bind({"alt", "shift"}, "down", function()
   cur:minimize()
 end)
 
-hs.hotkey.bind({"alt", "shift"}, "c", function()
+hs.hotkey.bind({"alt"}, "=", function()
+  local cur = hs.window.focusedWindow()
+  hs.grid.resizeWindowWider(cur)
+end)
+
+hs.hotkey.bind({"alt"}, "-", function()
+  local cur = hs.window.focusedWindow()
+  hs.grid.resizeWindowThinner(cur)
+end)
+
+hs.hotkey.bind({"alt", "shift"}, "=", function()
+  local cur = hs.window.focusedWindow()
+  hs.grid.resizeWindowTaller(cur)
+end)
+
+hs.hotkey.bind({"alt", "shift"}, "-", function()
+  local cur = hs.window.focusedWindow()
+  hs.grid.resizeWindowShorter(cur)
+end)
+
+hs.hotkey.bind({"alt"}, "c", function()
   local cur = hs.window.focusedWindow()
   print(cur)
+  --cur.setFrame(hs.geometry.rect(248,153,943,549))
   cur:centerOnScreen()
+end)
+
+
+hs.hotkey.bind({"alt"}, "return", function()
+  hs.application.launchOrFocus('iterm')
 end)
